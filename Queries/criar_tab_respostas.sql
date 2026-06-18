b96d08c7deed989922b099bc39eb9561f4f85730
Marcus Vinicius <mviniciusc93@gmail.com>
2026-06-18 16:03:25 -0300

    Ajustes na modelagem das tabelas respostas e itens. Criadas queries para ambos.

--- Queries/criar_tab_respostas.sql
+++ Queries/criar_tab_respostas.sql
@@ -11,9 +11,6 @@ CREATE TABLE Respostas (
     CO_PROVA_CH VARCHAR (10),
     CO_PROVA_MT VARCHAR (10),
     CO_PROVA_LC VARCHAR (10),
-    -- Vetores de texto com as respostas dos candidatos
-    TX_RESPOSTAS_CN VARCHAR(70),
-    TX_RESPOSTAS_CH VARCHAR(70),
-    TX_RESPOSTAS_MT VARCHAR(70),
-    TX_RESPOSTAS_LC VARCHAR(70),
+    -- Vetor com todas as respostas de cada candidato
+    TX_RESPOSTAS VARCHAR(200)
 );
--- criar_banco.py
+++ criar_banco.py
@@ -3,22 +3,61 @@
 
 import duckdb
 
-def criar_tabela_microdados():
+def criar_tabela_respostas():
     #conecta ao banco.
     con = duckdb.connect('enem_db.duckdb')
 
     # ler o .sql com o comando. Usa utf-8 para evitar problemas nos comentários
-    with open('Queries/criar_tab_microdados.sql', 'r', encoding='utf-8') as query:
+    with open('Queries/criar_tab_respostas.sql', 'r', encoding='utf-8') as query:
         query_criar_tabela = query.read()
 
     # executar a query acima
-    print('Atualizando tabela de microdados')
+    print('Atualizando tabela de respostas')
     con.execute(query_criar_tabela)
-    print('Tabela de microdados atualizada com sucesso.')
+    print('Tabela de respostas atualizada com sucesso.')
 
     # sempre fechar a conexão
     con.close()
 
+
+def criar_tabela_itens():
+    #conecta ao banco.
+    con = duckdb.connect('enem_db.duckdb')
+
+    # ler o .sql com o comando. Usa utf-8 para evitar problemas nos comentários
+    with open('Queries/criar_tab_itens.sql', 'r', encoding='utf-8') as query:
+        query_criar_tabela = query.read()
+
+    # executar a query acima
+    print('Atualizando tabela de itens')
+    con.execute(query_criar_tabela)
+    print('Tabela de itens atualizada com sucesso.')
+
+    # sempre fechar a conexão
+    con.close()
+
+
+def criar_tabela_questoes():
+    #conecta ao banco.
+    con = duckdb.connect('enem_db.duckdb')
+
+    # ler o .sql com o comando. Usa utf-8 para evitar problemas nos comentários
+    with open('Queries/criar_tab_questoes.sql', 'r', encoding='utf-8') as query:
+        query_criar_tabela = query.read()
+
+    # executar a query acima
+    print('Atualizando tabela de questoes')
+    con.execute(query_criar_tabela)
+    print('Tabela de questoes atualizada com sucesso.')
+
+    # sempre fechar a conexão
+    con.close()
+
+
+
+
 # só chamar a função que se quer executar
 
-criar_tabela_microdados()
+# criar_tabela_respostas()
+# criar_tabela_itens()
+# criar_tabela_questoes()
